d854d49edf0871b0e22d17d53171054100b9ff7d
Karin Prater <k.prater@tdsoftware.de>
2021-10-11 09:59:44 +0300

    Initial Commit

---
+++ MoviePlayer/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MoviePlayer
+//
+//  Created by Karin Prater on 11.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
